e708303d38582a21249af93b11bc19ced694b89d
Matthew99-alt <137392578+Matthew99-alt@users.noreply.github.com>
2024-05-29 16:31:15 +0500

    Delete simpcrm directory

--- simpcrm/src/main/java/com/crm/exception/PermissionDeniedException.java
+++
@@ -1,7 +0,0 @@
-package com.crm.exception;
-
-public class PermissionDeniedException extends RuntimeException {
-    public PermissionDeniedException(String message) {
-        super(message);
-    }
-}
